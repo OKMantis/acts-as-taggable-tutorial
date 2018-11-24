b95eafa066e040665be8df0ef18656b118db02ac
Owen Moss <owenkmoss@gmail.com>
2018-11-24 14:39:58 +0100

    generating scaffold restaurant

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_24_133611) do
+ActiveRecord::Schema.define(version: 2018_11_24_133749) do
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "taggings", force: :cascade do |t|
     t.integer "tag_id"
